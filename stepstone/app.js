cec2851414f6796fb96550a6dcf1b65cfae5c0e3
dworkz <dennis.sommer@sti2.at>
2018-11-16 22:59:22 +0100

    cors

--- stepstone/app.js
+++ stepstone/app.js
@@ -36,6 +36,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 // application.use("/public", express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
